1fbba219edcf1bb1f50d8e70d83a54941f3c1113
Dmitriy_Shelihov <Shelihovdima@yandex.ru>
2024-09-25 22:27:59 +0300

    added planes and lines

--- include/Triangles.h
+++ include/Triangles.h
@@ -26,10 +26,10 @@ class Point {
 };
 
 class Segment {
-	public:
+	private:
 		Point point1;
 		Point point2;
-	
+	public:
 		Snippet (Point _point1, Point _point2) 
 			: point1(_point1), point2(_point2) {}
 		double length () {
@@ -50,22 +50,95 @@ class Segment {
 			return (point.x-point1.x) * (point2.y-point1.y) == (point.y-point1.y) * (point2.x-point1.x) &&
 				   (point.y-point1.y) * (point2.z-point1.z) == (point.z-point1.z) * (point2.y-point1.y) &&
 				   (point.x-point1.x) * (point2.z-point1.z) == (point.z-point1.z) * (point2.x-point1.x) &&
-				   std::fabs(point.x-point2.x) <= std::fabs(point)
-									
+				   std::fabs(point.x-point2.x) <= std::fabs(point1.x-point2.x) && 
+				   std::fabs(point.x-point1.x) <= std::fabs(point1.x-point2.x) &&
+				   std::fabs(point.y-point2.y) <= std::fabs(point1.y-point2.y) &&
+				   std::fabs(point.y-point1.y) <= std::fabs(point1.y-point2.y) &&
+				   std::fabs(point.z-point2.z) <= std::fabs(point1.z-point2.z) &&
+				   std::fabs(point.z-point1.z) <= std::fabs(point1.z-point2.z);
 		}
-
+		bool point_belongs_same_segment_line(Point point) {
+			return (point.x-point1.x) * (point2.y-point1.y) == (point.y-point1.y) * (point2.x-point1.x) &&
+                   (point.y-point1.y) * (point2.z-point1.z) == (point.z-point1.z) * (point2.y-point1.y) &&
+                   (point.x-point1.x) * (point2.z-point1.z) == (point.z-point1.z) * (point2.x-point1.x);
+		}
+	
 }
 
 class Triangle {	
-	public: 
+	private: 
 		Point point1;
 		Point point2;
 		Point point3;
-
+	public:
 		Triangle (Point _point1, Point _point2, Point _point3) 
 			: point1(_point1), point2(_point2), point3(_point3) {}
 		bool valid() {
 			return point1.valid() && point2.valid() && point3.valid() && !point1.equal(point2) && !point1.equal(point3) && !point3.equal(point2);
 		}
+		void print() {
+			std::cout << "Triangle { ";
+			point1.print();
+			std::cout << ", ";
+			point2.print();
+			std::cout << ", ";
+			point3.print();
+			std::cout << " }";
+		}
+		bool degenerate_triangle() {
+			Segment side (point1, point2);
+			return side.point_belongs_same_segment_line(Point point3); 
+		}
+}
 
+class Line {
+	private:
+		Point point1;
+		Point point2;
+	public:
+		Line (Point _point1, Point _point2) 
+			: point1(_point1), point2(_point2) {}
+		bool valid() {
+			return point1.valid() && point2.valid() && !point1.equal(point2);
+		}
+		void print() {
+			std::cout << "Line { ";
+			point1.print();
+			std::cout << ", ";
+			point2.print();
+			std::cout << " }";
+		}
+		bool point_belongs_line(Point point) {
+        	return (point.x-point1.x) * (point2.y-point1.y) == (point.y-point1.y) * (point2.x-point1.x) &&
+                   (point.y-point1.y) * (point2.z-point1.z) == (point.z-point1.z) * (point2.y-point1.y) &&
+                   (point.x-point1.x) * (point2.z-point1.z) == (point.z-point1.z) * (point2.x-point1.x);
+        }
+
+}
+
+class Plane {
+	private:
+		Point point1;
+		Point point2;
+		Point point3;
+	public:
+		Plane (Point _point1, Point _point2, Point _point3)
+			: point1(_point1), point2(_point2), point3(_point3) {}
+		bool valid() {
+			Line line (point1, point2);
+			return point1.valid() && point2.valid() && point3.valid() && line.point_belongs_line(point3);
+		}
+		void print() {
+         	std::cout << "Plane { ";
+          	point1.print();
+          	std::cout << ", ";
+          	point2.print();
+          	std::cout << ", ";
+          	point3.print();
+          	std::cout << " }";
+		}
+		
+		Line plane_intersection(Plane plane2) {
+					
+		}
 }
